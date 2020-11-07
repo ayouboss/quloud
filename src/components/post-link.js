a4e4eec7ad8ee2042d3e3e919dcca73b5cbb7979
ayouboss <ghzala.ayoub@gmail.com>
2020-11-07 17:12:31 -0500

    Update post-link.js

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -10,9 +10,9 @@ const PostLink = ({ post }) => (
     </Link>
     <header>
       <h2 className="post-title">
-        <Link to={post.frontmatter.path} className="post-link">
+       
           {post.frontmatter.title}
-        </Link>
+ 
       </h2>
       
       <p>{post.frontmatter.metaDescription}</p>
